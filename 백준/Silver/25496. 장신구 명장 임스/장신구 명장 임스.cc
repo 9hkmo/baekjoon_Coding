dc3384dfdce4dfb92b4c666045d7195efe060fb5
9hkmo <gurah129@gmail.com>
2025-02-02 02:29:32 +0900

    [Silver V] Title: 장신구 명장 임스, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/25496. 장신구 명장 임스/장신구 명장 임스.cc
+++ 백준/Silver/25496. 장신구 명장 임스/장신구 명장 임스.cc
@@ -4,21 +4,18 @@
 using namespace std;
 
 int main() {
-	int N, P, num, max;
+	int N, P, num, i;
 	vector<int> A;
 	cin >> P >> N;
-	for (int i = 0; i < N; i++) {
+	for (i = 0; i < N; i++) {
 		cin >> num;
 		A.push_back(num);
 	}
 	sort(A.begin(), A.end());
 
-	max = 0;
-	for (int i = 0; P < 200 && i < N; i++) {
+	for (i = 0; P < 200 && i < N; i++) 
 		P += A[i];
-		max++;
-	}
-	cout << max;
+	cout << i;
 	
 	return 0;
 }

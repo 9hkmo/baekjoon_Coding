01ad30f4953d2d4c387a26e9904fd8759a115169
9hkmo <gurah129@gmail.com>
2025-02-04 17:27:01 +0900

    [Silver I] Title: 안정적인 문자열, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/4889. 안정적인 문자열/안정적인 문자열.cc
+++ 백준/Silver/4889. 안정적인 문자열/안정적인 문자열.cc
@@ -1,31 +1,23 @@
 #include <iostream>
-#include <stack>
 #include <string>
 using namespace std;
 
 int main() {
 	string str;
-	int n;
+	int i, j, n, stack;
 
-	for (int i = 1; ; i++) {
-		stack<char> S;
-		n = 0;
+	for (i = 1;; i++) {
+		stack = n = 0;
 		cin >> str;
 		if (str[0] == '-') break;
-		for (int j = 0; j < str.length(); j++) {
-			if(str[j] == '{')
-				S.push(str[j]);
-			
-			else if (str[j] == '}') {
-				if (!S.empty())
-					S.pop();
-				else {
-					S.push('{');
-					n++;
-				}
-			}
+
+		for (j = 0; j < str.length(); j++) {
+			if (str[j] == '{')
+				stack++;
+			else
+				stack ? stack-- : (n++, stack++);
 		}
-		n += S.size() / 2;
+		n += stack / 2;
 		cout << i << ". " << n << endl;
 	}
 	return 0;

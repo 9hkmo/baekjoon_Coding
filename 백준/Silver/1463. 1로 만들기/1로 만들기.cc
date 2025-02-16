ed34253f97284eb7a1ef7e5b6992f63b51acc51f
9hkmo <gurah129@gmail.com>
2025-02-17 02:00:55 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -5,11 +5,9 @@ int main() {
 	int N;
 	cin >> N;
 
-	int *dp = new int[N + 3];
+	int *dp = new int[N + 1];
 	dp[1] = 0;
-	dp[2] = 1;
-	dp[3] = 1;
-	for (int i = 4; i <= N; i++) {
+	for (int i = 2; i <= N; i++) {
 		dp[i] = dp[i - 1] + 1;
 		if (i % 3 == 0) dp[i] = min(dp[i / 3] + 1, dp[i]);
 		if (i % 2 == 0) dp[i] = min(dp[i / 2] + 1, dp[i]);

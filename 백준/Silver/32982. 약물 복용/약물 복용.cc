0070af023d705b6cb4aeb124efa786c32a62cc5f
9hkmo <gurah129@gmail.com>
2025-03-03 19:49:55 +0900

    [Silver II] Title: 약물 복용, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/32982. 약물 복용/약물 복용.cc
+++ 백준/Silver/32982. 약물 복용/약물 복용.cc
@@ -6,24 +6,20 @@ typedef struct Time {
 }Time;
 
 int main() {
-    int N, K, i, temp;
+    int N, K, i, j, temp;
     Time A[3];
     cin >> N >> K;
     for (i = 0; i < 3; i++) cin >> A[i].start >> A[i].end;
     temp = A[0].end;
 
     for (i = 0; i < N; i++) {
-        if (temp + K < A[1].start) {
-            cout << "NO";
-            return 0;
+        for (j = 0; j < 2; j++) {
+            if (temp + K < A[j+1].start) {
+                cout << "NO";
+                return 0;
+            }
+            temp = temp + K < A[j+1].end ? temp + K : A[j+1].end;
         }
-        temp = temp + K < A[1].end ? temp + K : A[1].end;
-
-        if (temp + K < A[2].start) {
-            cout << "NO";
-            return 0;
-        }
-        temp = temp + K < A[2].end ? temp + K : A[2].end;
 
         if (i < N - 1) {
             if (temp + K < A[0].start + 1440) {
